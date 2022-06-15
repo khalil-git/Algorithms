92dc6be2a9533579e57090bd2d881190d88c9058
Mohamed Khalil <mkhalil.alex@gmail.com>
2022-06-15 13:42:01 +0200

    Permutaion

---
+++ permutation/permutaion.js
@@ -0,0 +1,44 @@
+const permutation = {
+
+    // Get permutaion list of a giving string
+    permutate: function (str="", temp =[], result=[]){
+    
+            const arr = [...str];     
+    
+            if(arr.length === 0){	
+            result.push([...temp].join(''));  
+            return;
+            }
+                
+            
+            for(let i in arr){
+    
+            let curr = arr[i];
+
+            //push current character  
+            temp.push(curr);     
+            //get rest char
+            const rest = arr.filter((item, index) =>{
+                return index != i;
+            })        
+
+            this.permutate(rest, temp, result)
+    
+            //remove last char from temp array
+            temp.pop();
+    
+            }
+    
+            return result;
+    },
+    //Get the count of the avilable permuation
+    count: function(str){
+            let c = this.permutate(str).length;	 
+            return c;
+    }
+  }  
+  
+   
+  console.log(permutation.count("abcd"));
+  
+  
